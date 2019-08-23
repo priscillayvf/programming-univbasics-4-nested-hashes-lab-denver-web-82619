e81a5373fdd20a67004ba7ab60ae87a352983718
Priscilla Firpo <priscillayf@gmail.com>
2019-08-23 01:34:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,11 +62,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-  programmer_hash[:yukihiro_matsumoto] = {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  }
-
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
@@ -88,6 +83,11 @@ def adding_matz
           :languages => ["C"]
         }
     }
+  
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
 
     
 end
