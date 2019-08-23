04c3eef0c5e1e4d4bf3f8d91c213996d232e870d
Priscilla Firpo <priscillayf@gmail.com>
2019-08-23 01:21:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,8 +21,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	programmer_hash[:alan_kay][:known_for]
-	
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
@@ -40,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  	programmer_hash[:alan_kay][:known_for]
+  	
 end
 
 def dennis_ritchies_language
